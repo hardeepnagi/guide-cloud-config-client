b23e789ca2e980c85a6ecfacd6a35e33fab7dde9
Martin Jensen <martin2610@gmail.com>
2016-04-04 17:32:52 +0200

    Added project

---
+++ src/main/java/com/rpicloud/ConfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+public class ConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigclientApplication.class, args);
+	}
+}
