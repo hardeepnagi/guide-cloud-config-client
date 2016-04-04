02ca091799eaa534bbeee9747305cb0003ecd587
Martin Jensen <martin2610@gmail.com>
2016-04-04 19:38:48 +0200

    Greeter added

--- src/main/java/com/rpicloud/ConfigclientApplication.java
+++ src/main/java/com/rpicloud/ConfigclientApplication.java
@@ -1,8 +1,14 @@
 package com.rpicloud;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class ConfigclientApplication {
@@ -10,4 +16,5 @@ public class ConfigclientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
 	}
+
 }
